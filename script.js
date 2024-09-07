5ab4acd25644a31384a9250994a6e550ebde71b8
Nguyen My Thong <90298623+iknizzz1807@users.noreply.github.com>
2024-09-07 11:03:22 +0700

    Removed the wait for font load cause it is dumb

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
-Promise.all([
-  document.fonts.ready,
-  new Promise((resolve) => {
-    if (document.readyState !== "loading") {
-      resolve();
-    } else {
-      document.addEventListener("DOMContentLoaded", resolve);
-    }
-  }),
-])
-  .then(() => {
-    console.log("Fonts loaded and DOM ready");
-    const title = document.querySelector(".title");
-    const nav = document.querySelector(".navigations");
+document.addEventListener("DOMContentLoaded", (event) => {
+  const title = document.querySelector(".title");
+  const nav = document.querySelector(".navigations");
 
-    function calculateTypingDuration(text) {
-      return text.length * 0.75 * 50; // 50ms per character
-    }
+  // Calculate the duration of the typing animation
+  const textLength = title.textContent.length;
+  const typingDuration = textLength * 0.75 * 50; // 50ms per character
 
-    const typingDuration = calculateTypingDuration(title.textContent);
-
-    // Show navigation after typing animation
-    setTimeout(() => {
-      requestAnimationFrame(() => {
-        nav.classList.remove("hidden");
-        nav.classList.add("fade-in");
-      });
-    }, typingDuration);
-  })
-  .catch((error) => {
-    console.error("An error occurred:", error);
-  });
+  // Show navigation after typing animation
+  setTimeout(() => {
+    nav.classList.remove("hidden");
+    nav.classList.add("fade-in");
+  }, typingDuration);
+});
