7bcd20fac1b71e08102d276190a182461a301170
Nguyen My Thong <90298623+iknizzz1807@users.noreply.github.com>
2024-09-07 10:48:44 +0700

    Add wait for font load finished

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  const title = document.querySelector(".title");
-  const nav = document.querySelector(".navigations");
+Promise.all([
+  document.fonts.ready,
+  new Promise((resolve) => {
+    if (document.readyState !== "loading") {
+      resolve();
+    } else {
+      document.addEventListener("DOMContentLoaded", resolve);
+    }
+  }),
+])
+  .then(() => {
+    console.log("Fonts loaded and DOM ready");
+    const title = document.querySelector(".title");
+    const nav = document.querySelector(".navigations");
 
-  // Calculate the duration of the typing animation
-  const textLength = title.textContent.length;
-  const typingDuration = textLength * 0.75 * 50; // 50ms per character
+    function calculateTypingDuration(text) {
+      return text.length * 0.75 * 50; // 50ms per character
+    }
 
-  // Show navigation after typing animation
-  setTimeout(() => {
-    nav.classList.remove("hidden");
-    nav.classList.add("fade-in");
-  }, typingDuration);
-});
+    const typingDuration = calculateTypingDuration(title.textContent);
+
+    // Show navigation after typing animation
+    setTimeout(() => {
+      requestAnimationFrame(() => {
+        nav.classList.remove("hidden");
+        nav.classList.add("fade-in");
+      });
+    }, typingDuration);
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
